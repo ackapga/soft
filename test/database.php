4700c4cb403e01f5a109696fa16650a10618655e
ackapga2 <ackapga@gmail.com>
2023-12-09 12:41:10 +0600

    09.12 новый скрипт для новых таблиц

---
+++ test/database.php
@@ -0,0 +1,48 @@
+<?php
+
+$servername = "localhost";
+$username = "v_20478_Gulbanu";
+$password = "dala3940";
+$dbname = "v_20478_metal";
+
+// Создаем подключение к базе данных
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Проверяем успешность подключения
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// Массив с разными именами таблиц
+$tableNames = ["ASQ1", "ASQ2"];
+
+// Общие параметры для создания таблицы
+$commonColumns = "(
+  `id` INT(9) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `parent_id` MEDIUMINT(9) UNSIGNED NOT NULL DEFAULT '0',
+  `first_parent` MEDIUMINT(9) UNSIGNED NOT NULL DEFAULT '0',
+  `date` INT(10) UNSIGNED NOT NULL,
+  `theme_id` SMALLINT(6) UNSIGNED NOT NULL,
+  `login` varchar(30) collate utf8_unicode_ci NOT NULL,
+  `message` varchar(9999) collate utf8_unicode_ci NOT NULL,
+  `rating` INT(5) collate utf8_unicode_ci NOT NULL,
+  `moderation` TINYINT(3) UNSIGNED NOT NULL DEFAULT '0',
+  `plus` MEDIUMINT(9) NOT NULL DEFAULT '0',
+  `minus` MEDIUMINT(9) NOT NULL DEFAULT '0',
+  PRIMARY KEY  (`id`),
+  KEY `theme_id` (`theme_id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1;";
+
+// Создаем таблицы с разными именами
+foreach ($tableNames as $tableName) {
+    $sql = "CREATE TABLE IF NOT EXISTS $tableName $commonColumns";
+
+    if ($conn->query($sql) === TRUE) {
+        echo "Таблица $tableName успешно создана" . PHP_EOL;
+    } else {
+        echo "Ошибка при создании таблицы $tableName: " . $conn->error . "<br>";
+    }
+}
+
+// Закрываем соединение с базой данных
+$conn->close();
