11eed6084061a5bd7c9cdbf2d25ecf9e57267af6
ackapga2 <ackapga@gmail.com>
2023-12-15 17:57:05 +0600

    15.12 More FIXX

--- js/swiper.custom.js
+++ js/swiper.custom.js
@@ -1,47 +1,12 @@
-// Swiper Slider
-var TrandingSlider = new Swiper('.tranding-slider', {
-    effect: 'coverflow',
-    grabCursor: true,
-    centeredSlides: true,
-    loop: true,
-    spaceBetween: 1,
-    slidesPerView: 'auto',
-    coverflowEffect: {
-        rotate: 0,
-        stretch: 0,
-        depth: 100,
-        modifier: 2.5,
-    },
-    pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
-    },
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-    }
-});
-
-// const animationAuto = setInterval(function () {
-//     document.querySelector('.swiper-button-next').click();
-// }, 1000);
-//
-// document.querySelector('.tranding-slider-control').addEventListener("click", function () {
-//     clearInterval(animationAuto);
-// });
-
-
 // Region Slider
 const images = [
     'region-shimkent.png',
     'region-almaty.png',
     'region-astana.png',
 ];
-
 const nameCity = [
-    'ШЫМКЕНТ','АЛМАТЫ','АСТАНА',
+    'ШЫМКЕНТ', 'АЛМАТЫ', 'АСТАНА',
 ];
-
 let activeImage = 0;
 const sliderPlace = document.querySelector('.region-img');
 const widthOffset = document.querySelector('.region-slider').clientWidth;
@@ -49,8 +14,6 @@ sliderPlace.style.width = 3 * widthOffset + "px";
 sliderPlace.style.height = widthOffset + "px";
 sliderPlace.style.left = '-' + widthOffset + "px";
 let flag = true;
-
-
 const initSlider = () => {
     const img = document.createElement('img');
     img.alt = '';
@@ -58,7 +21,6 @@ const initSlider = () => {
     sliderPlace.append(img);
     nextImagesGeneration();
 }
-
 const nextImagesGeneration = () => {
     let nextImages = activeImage + 1;
     if (nextImages >= images.length && nextImages >= nameCity.length) nextImages = 0;
@@ -68,7 +30,6 @@ const nextImagesGeneration = () => {
     document.querySelector('.region-name').innerHTML = nameCity[nextImages];
     sliderPlace.append(img);
 }
-
 const nextSlide = () => {
     if (!flag) return;
     flag = !flag;
@@ -79,18 +40,13 @@ const nextSlide = () => {
         duration: 1000,
         draw: function (progress) {
             // document.querySelector('.region-img img').style.width = (widthOffset * (1 - progress)) + "px";
-             document.querySelector('.region-img img').style.width = (widthOffset * (1 - progress)) + "px";
+            document.querySelector('.region-img img').style.width = (widthOffset * (1 - progress)) + "px";
         },
         removeElement: document.querySelector('.region-img img')
     });
 }
-
 initSlider();
-
-
 document.querySelector('.region-right').addEventListener('click', nextSlide);
-
-
 const animate = ({duration, draw, removeElement}) => {
     const start = performance.now();
 
@@ -106,3 +62,27 @@ const animate = ({duration, draw, removeElement}) => {
         }
     });
 }
+
+// Swiper Slider
+// var TrandingSlider = new Swiper('.tranding-slider', {
+//     effect: 'coverflow',
+//     grabCursor: true,
+//     centeredSlides: true,
+//     loop: true,
+//     spaceBetween: 1,
+//     slidesPerView: 'auto',
+//     coverflowEffect: {
+//         rotate: 0,
+//         stretch: 0,
+//         depth: 100,
+//         modifier: 2.5,
+//     },
+//     pagination: {
+//         el: '.swiper-pagination',
+//         clickable: true,
+//     },
+//     navigation: {
+//         nextEl: '.swiper-button-next',
+//         prevEl: '.swiper-button-prev',
+//     }
+// });
